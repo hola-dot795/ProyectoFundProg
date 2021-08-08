4b9ee2d811cc91ded1c4bf99327e024905a34a69
AngelVelascoJr <80130331+AngelVelascoJr@users.noreply.github.com>
2021-08-07 23:45:32 -0500

    LogIn

--- Test1.cpp
+++ Test1.cpp
@@ -10,15 +10,16 @@ struct Jugador
 	int JuegosJugados;
 	int JuegosGanados;
 };
-int n = 1, Op;
+int n = 1, Op, UserIndex, ArraySize;
 char Nombre[20], Contra[20];
-Jugador *Users, *User;
+Jugador *Users/*, *User*/;
 FILE *DataFile;
 
 void Play();
 void LogIn();
 void Stadistics();
 void CreateProfile();
+void DataFromFile();
 main()
 {	
 	do
@@ -50,7 +51,35 @@ main()
 void LogIn()
 {
 	DataFromFile();
-		
+	char VarUsName[20], VarPassW[20];
+	
+	bool IsValid = false;
+	//printf("%i, %i, %i", ArraySize, VarInArraySize, Size);
+	do
+	{
+		fflush(stdin);
+		printf("Dame tu usuario: ");
+		fflush(stdin);
+		gets(VarUsName);
+		printf("Dame tu contraseña: ");
+		gets(VarPassW);
+		for(int i = 0; i < ArraySize; i++)
+		{
+			if(IsValid == false)
+			{
+				printf("Comparando %s con %s", VarUsName, Users[i].Username);
+				if(strcmp(Users[i].Username, VarUsName) != 0 || strcmp(Users[i].Password, VarPassW))
+				{
+					IsValid == true;
+					UserIndex = i;
+					/*User[0].JuegosJugados == Users[i].JuegosJugados;
+					User[0].JuegosGanados == Users[i].JuegosGanados;
+					User[0].Password == Users[i].Password;
+					User[0].Username == Users[i].Username;*/
+				}
+			}
+		}
+	}while(IsValid == false);
 }
 
 void CreateProfile()
@@ -70,17 +99,29 @@ void Stadistics()
 
 void DataFromFile()	//carga el archivo de perfiles
 {
-	Users = (Jugador*)malloc(sizeof(Jugador));
 	DataFile = fopen("file.txt" , "r");
 	if(DataFile == NULL) 
 	{
 		perror("Error opening file");
 	}
-	printf("asdased");
-	int i = 0;
-	while(feof(DataFile) == NULL)
+	int i = 1;
+	for (char c = getc(DataFile); c != EOF; c = getc(DataFile))
+	{
+		if (c == '\n')
+		{
+			i++;
+			//printf("Line Found");
+		}
+		//printf("%c", c);
+	}
+	//printf("\n");
+	Users = (Jugador*)malloc(i*sizeof(Jugador));
+	ArraySize = (i);
+	//printf("%i\n", ArraySize);
+	while(!feof(DataFile))
 	{
 		fscanf(DataFile,"%s %s %i %i\n", &Users[i].Username, &Users[i].Password, &Users[i].JuegosJugados, &Users[i].JuegosGanados);
+		printf("%s %s %i %i\n", &Users[i].Username, &Users[i].Password, &Users[i].JuegosJugados, &Users[i].JuegosGanados);
 		i++;
 	}
 }
